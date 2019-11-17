ff0bbf1397352015b8d5400d37c1c68738ecf80a
vansuphan <37709216+vansuphan@users.noreply.github.com>
2019-11-17 09:49:51 +0700

    add cmt auth login

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,10 +6,10 @@ const Product = require('../models/product.model');
 router.post('/', productController.postProduct );
 
 router.get('/',(req,res)=>{
-    res.send('No data');
-}
     var idproduct = req.body.id
-    if(!idproduct)
+    if(!idproduct){
+        res.send('No data');
+    }
     let product = Product.findOne({
         idproduct : idproduct
     });
